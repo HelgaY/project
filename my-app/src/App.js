2654305c22905330acf72edc8728d46c037747f4
HelgaY <olga.v.astafieva@ya.ru>
2019-08-04 17:09:34 +0300

    1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div class="form-group container">
+function App(props) {
+  return <div class="container">
       <div class="row justify-content-md-center">
-          <form class="form-group">
-            <label for="login">Логин</label>
-            <input class="form-control" type="text" id="login" name="login" value=""></input>
-            <label for="passw">Пароль</label>
-            <input class="form-control" type="text" id="passw" name="passw" value=""></input>
-            <input class="btn btn-primary" type="submit" value="Войти"></input>
+          <form>
+            <div class="form-group row">
+              <label for="login" class="col-sm-3 col-form-label">Логин</label>
+              <div class="col-sm-9">
+                <input class="form-control" type="text" id="login" name="login" value={props.login} placeholder="Логин" />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="passw" class="col-sm-3 col-form-label">Пароль</label>
+              <div class="col-sm-9">
+                <input class="form-control" type="text" id="passw" name="passw" value={props.passw} placeholder="Пароль" />
+              </div>
+            </div>
+            <div class="form-group row">
+              <div class="col-sm-6">
+                <input class="btn btn-block btn-outline-secondary" type="submit" name="btn" value="Регистрация" />
+              </div>
+              <div class="col-sm-6">
+              <input class="btn btn-block btn-outline-primary" type="submit" name="btn" value="Войти" />
+              </div>
+            </div>
           </form>
         </div>
-    </div>
-  );
+    </div>;
 }
 
 export default App;
